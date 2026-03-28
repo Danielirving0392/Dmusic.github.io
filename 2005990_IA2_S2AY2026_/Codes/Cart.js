75ec0046fe629dcf045be17f55553b485344721d
Danielirving0392 <danielpirving@students.utech.edu.jm>
2026-03-28 13:07:14 -0500

    Delete 2005990_IA2_S2AY2026_ directory

--- 2005990_IA2_S2AY2026_/Codes/Cart.js
+++
@@ -1,8 +0,0 @@
-// Cart page initialisation
-// Shared cart functions (renderCart, removeFromCart, updateQty, getCart, saveCart)
-// are defined in Shop.js, which is loaded before this file in Cart.html
-
-window.onload = function () {
-    // Populate the cart table when the page loads
-    renderCart();
-};
